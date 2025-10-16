db066c84825263d2c0ac5235df1962a613c87f3d
eurlcernunnos-star <eurlcernunnos@gmail.com>
2025-10-16 17:53:37 +0200

    Add Tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      colors: { hap: {50:'#f0f8ff',100:'#dff0ff',200:'#bfe1ff',300:'#9fd3ff',400:'#7fc4ff',500:'#5fb6ff',600:'#3699f0',700:'#1f7bd1',800:'#165ea1',900:'#123f6a'} },
+      colors: {
+        hap: {50:'#f0f8ff',100:'#dff0ff',200:'#bfe1ff',300:'#9fd3ff',400:'#7fc4ff',500:'#5fb6ff',600:'#3699f0',700:'#1f7bd1',800:'#165ea1',900:'#123f6a'}
+      },
       boxShadow:{ glow:'0 0 0 6px rgba(95,182,255,0.15)'},
       borderRadius: { '2xl':'1rem' }
     },
